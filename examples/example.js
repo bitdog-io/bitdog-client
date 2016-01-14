2c63ac51d5ccacfbbe65263d1911a6045a08643b
Vincent Miceli <vjm@orchardparksoftware.com>
2016-01-14 11:28:11 -0500

    Unremarked data collector example

--- examples/example.js
+++ examples/example.js
@@ -14,14 +14,14 @@ bitdog.addCommand('Turn light on/off',  bitdog.commonMessageSchemas.onOffMessage
 
 
 // Create a new data collector for our position sensor
-//var positionDataCollector = bitdog.addDataCollector('Position', bitdog.commonMessageSchemas.mapPositionMessageSchema , 10000, function (message, configuration, logger) {
+var positionDataCollector = bitdog.addDataCollector('Position', bitdog.commonMessageSchemas.mapPositionMessageSchema , 10000, function (message, configuration, logger) {
     
-//    // Instead of collecting real data, we are just sending random data
-//    // for this test
-//    message.latitude = Math.floor((Math.random() * 100) + 1);
-//    message.longitude = Math.floor((Math.random() * 100) + 1);
+    // Instead of collecting real data, we are just sending random data
+    // for this test
+    message.latitude = Math.floor((Math.random() * 100) + 1);
+    message.longitude = Math.floor((Math.random() * 100) + 1);
    
-//});
+});
 
 // Create a new subscription
 bitdog.addSubscription('*', 'Position', 'add', function (message, configuration, logger) {
@@ -44,6 +44,15 @@ bitdog.on('ready', function (logger, configuration) {
 
 
     //},15 * 60000);
+
+    //setInterval(function () {
+    //    isOn = !isOn;
+    //    bitdog.sendCommand('bfed21e0-6822-423e-a1f4-3b4d98bdc2cf', 'Turn light on/off', bitdog.commonMessageSchemas.onOffMessageSchema, function (message) {
+    //        message.value = isOn ? 'off' : 'on';
+    //    });
+
+
+    //}, 15000);
   
 });
 
