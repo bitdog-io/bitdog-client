3e52a2abf484ce6c1b144231ab673d49fe264d43
Vincent Miceli <vjm@orchardparksoftware.com>
2016-09-13 10:05:14 -0400

    Command results

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,7 @@
+/// <reference path="globals/commander/index.d.ts" />
+/// <reference path="globals/logrotate-stream/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
+/// <reference path="globals/nconf/index.d.ts" />
+/// <reference path="globals/node-uuid/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/temp/index.d.ts" />
